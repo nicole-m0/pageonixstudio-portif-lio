534bfa560cba6ab09061e86bff62efba628fda40
nicole-m0 <nicolemendes.dev@gmail.com>
2026-03-26 18:00:05 -0300

    commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 // Scroll suave
-  document.querySelectorAll('a[href^="#"]').forEach(link => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      target.scrollIntoView({ behavior: 'smooth' });
-    });
+  const navLinks = document.querySelectorAll('#menu a.link');
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      const headerHeight = document.querySelector('header').offsetHeight;
+      const targetPosition = target.offsetTop - headerHeight - 20;
+      window.scrollTo({
+        top: targetPosition,
+        behavior: 'smooth'
+      });
+    }
   });
+});
 
   //menu
   const btn = document.getElementById("menu-btn");
